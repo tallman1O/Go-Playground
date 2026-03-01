ff8c809cac97f246c3c49b0e2046217023226a68
Mehul Uttam <mehulsavio@gmail.com>
2026-03-01 16:22:17 +0530

    variables in go

---
+++ 3_variables/variable.go
@@ -0,0 +1,16 @@
+package main
+
+var c, pyhon, java bool
+
+func main() {
+
+	var b string
+	a := 7 // same as var a int = 7
+
+	println(a, b, c, pyhon, java)
+}
+
+// Rules :
+// 1. Variable names must start with a letter or an underscore.
+// 2. Use := for local variables (Inside functions)
+// 3. Use var for package-level variables (Outside functions)
